72a7635b931e2dc073b64ab57df5b86b31d3f1d5
Taqi Niazi <taqiniazi409@gmail.com>
2024-11-04 16:16:49 +0530

    added some styling & heading

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,15 @@
 import Projects from "./Projects";
 import "./styleHome.css";
 
-function Home(){
-    return <>
-    <div className="homeDiv">
-    <h1 className="homeh1">React Projects</h1>
-    </div>
-    
-    <Projects/>
-    </>
+function Home() {
+    return (
+        <>
+            <div className="homeDiv">
+                <h1 className="homeh1">React Projects</h1>
+            </div>
+            <Projects />
+        </>
+    );
 }
+
 export default Home;

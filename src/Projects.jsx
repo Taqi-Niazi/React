a6d78403a032ad236f089d41145b937f73f78543
Taqi Niazi <taqiniazi409@gmail.com>
2024-12-04 20:05:19 +0530

    updated titles

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -9,7 +9,7 @@ function Projects(){
          <button className='prbutton'><Link to="/ColorPick"><p  className="medium">ColorPick</p></Link></button>
          <button className='prbutton'><Link to="/DigitalClock"><p  className="large">DigitalClock</p></Link></button>
          <button className='prbutton'><Link to="/Flower"><p className='small'>Flower</p></Link></button>
-         <button className='prbutton'><Link to="/Incdecr"><p  className="medium">Incdecr</p></Link></button>
+         <button className='prbutton'><Link to="/Incdecr"><p  className="medium">Inc-decr</p></Link></button>
          <button className='prbutton'><Link to="/OnChange"><p className='small'>Order</p></Link></button>
          <button className='prbutton'><Link to="/StopWatch"><p  className="large">StopWatch</p></Link></button>
          <button className='prbutton'><Link to="/ToDoList"><p className='medium'>ToDoList</p></Link></button>
